0431cc8c353fae17f507831076fbf545ccc1f678
ismailbozan90 <ismailbozan90@gmail.com>
2024-10-04 18:43:45 +0300

    first

---
+++ src/main/java/org/scamlet/mvc/mvcstock/MvcStockApplication.java
@@ -0,0 +1,13 @@
+package org.scamlet.mvc.mvcstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcStockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvcStockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/scamlet/mvc/mvcstock/MvcStockApplicationTests.java
@@ -0,0 +1,13 @@
+package org.scamlet.mvc.mvcstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcStockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
